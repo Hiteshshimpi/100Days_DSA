e9b5eecbdab00ecd215b3ecf84b666eb64a8ec48
Hitesh Shimpi <hitesh.shimpi@oracle.com>
2025-08-27 18:15:19 +0530

    Practice1 prime number , even odd and leap year

--- src/Practice1.java
+++ src/Practice1.java
@@ -2,10 +2,10 @@ public class Practice1 {
     public static void main(String[] args){
        boolean val =prime1(5);
         System.out.println(val);
+        evenOddNum(3);
+        vowelsOrConstants('a');
+        leapYear(2009);
     }
-
-
-
     public static boolean prime1(int num){
             if(num <=1)
             {
@@ -15,5 +15,44 @@ public class Practice1 {
                 return false;
             }
         return true;
+    }
+
+    // Homework day 1 Even and Odd
+
+    public static void evenOddNum(int num)
+    {
+        for(int i =1;i<=num;i++){
+            if(i % 2 ==0)
+            {
+                System.out.println("It is a Even Number: "+i);
+            }
+            else
+            {
+                System.out.println("It is a Odd Number: "+i);
+            }
+        }
+    }
+    // Homework day 1 Vowels and constants
+    public static void vowelsOrConstants(char ch)
+    {
+        if (ch=='a' || ch=='e' || ch=='i' || ch=='o'|| ch=='u'){
+            System.out.println("Alphabet "+ch +" is Vowel");
+        }
+        else {
+            System.out.println("Alphabet "+ch +" is Constant");
+        }
+    }
+
+    // Homework day 1 LeapYear
+
+    public  static  void leapYear(int year)
+    {
+        if((year % 4==0 && year %100!=0)|| (year % 400 ==0)){
+            System.out.println(year +" this year is a leap year");
+        }
+        else
+        {
+            System.out.println(year +" this year is not a leap year");
+        }
     }
 }

cf57883d86e77a14e485088950b98cd27578d1f4
Hitesh Vinod Shimpi <43511832+hiteshshimpi@users.noreply.github.com>
2025-09-17 22:45:48 +0530

    fIXING PREVIOUS CODE

--- src/MaxSumArray.java
+++ src/MaxSumArray.java
@@ -1,8 +1,30 @@
+import java.util.HashMap;
+
 public class MaxSumArray {
 
     //Input: nums = [1,1,1], k = 2
     //Output: 2
     public static void main(String[] args) {
             int[] nums={1,1,1};
+        HashMap<Integer,Integer> hash= new HashMap<>();
+            int count=0;
+            int sum=0;
+            int k=2;
+            for(int i=0;i<nums.length;i++)
+            {
+                sum =sum+nums[i];
+                if(sum==k) count++;
+                if(hash.containsKey(sum-k)){
+                    count =count+hash.get(sum-k);
+                }
+                else
+                {
+                    hash.put(sum,hash.getOrDefault(sum,0)+1);
+                }
+            }
+
+        System.out.println(hash);
+        System.out.println(count);
+
     }
 }

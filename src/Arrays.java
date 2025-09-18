321321ac859a7a8c3ed7d54a060bbc096e69256b
Hitesh Vinod Shimpi <43511832+hiteshshimpi@users.noreply.github.com>
2025-09-18 23:12:15 +0530

    Sum and reverse array

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 public class Arrays {
     public static void main(String[] args) {
         // Running sum of arrays 1408 leetcode
@@ -23,5 +21,47 @@ public class Arrays {
         System.out.println(sum);
         System.out.println(java.util.Arrays.toString(arr));
 
+    }
+
+    public static class SumofAllElementsTwoD {
+        public static void main(String[] args) {
+            int[] [] arr ={{1,2,3},{4,5,6}};
+            int sum=0;
+            for(int[] row : arr)
+                for(int val :row)
+                    sum=sum+val;
+
+            System.out.println("Optimzed method"+sum);
+
+            sum=0;
+            for(int i=0;i<arr.length;i++)
+            {
+                for (int col=0;col<arr[0].length;col++)
+                {
+                    sum= sum+arr[i][col];
+                }
+            }
+            System.out.println(sum);
+        }
+
+    }
+
+    public static class ReverseArray {
+        public static void main(String[] args) {
+            int[] arr={1, 2, 3, 4, 5};
+            int i=0;
+            int j=arr.length-1;
+
+            while (i<j)
+            {
+                int temp = arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                i++;
+                j--;
+            }
+
+            System.out.println(java.util.Arrays.toString(arr));
+        }
     }
 }

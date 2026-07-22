30d8455fc4d2f5493a02ab16c2ff2e33528d00e8
Hitesh Shimpi <hiteshshimpi2310@gmail.com>
2026-07-22 19:43:22 +0530

    Decode String Optimized Version/ Solution

--- src/Revise/DecodeStringOptimized.java
+++ src/Revise/DecodeStringOptimized.java
@@ -1,4 +1,18 @@
 package Revise;
 
 public class DecodeStringOptimized {
+     public  static void main(String [] args) {
+         String input ="d3m4t2b0a1";   // Output: dddmmmmttaa
+         StringBuilder output = new StringBuilder();
+         // now we would consider the input string in pairs of character and digit
+
+         for(int i =0;i<input.length();i=i+2) //why +2 , as even index is char and odd is digit
+         {
+             char ch = input.charAt(i);
+             int count = input.charAt(i+1)-'0';
+             output.repeat(String.valueOf(ch), Math.max(0, count));
+
+         }
+         System.out.println(output);
+    }
 }
